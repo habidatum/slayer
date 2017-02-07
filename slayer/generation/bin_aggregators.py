d858174c4c3bc4967d4cf4c204421da25161041a
Nikita Pestrov <ipestriov@gmail.com>
2017-02-07 11:56:39 +0300

    Support NaNs in the absence of measurements

--- slayer/generation/bin_aggregators.py
+++ slayer/generation/bin_aggregators.py
@@ -4,12 +4,12 @@ import numpy as np
 def weighted_mean(index, slice_area, counts, weights):
     weighted = np.bincount(index, minlength=slice_area, weights=weights)
     weighted_counts = weighted/counts
-
-    nans_index = np.isnan(weighted_counts)
-    weighted_counts[nans_index] = 0.0
+    # The NaNs here represent the places with no measurements
     return weighted_counts
 
 
 def weighted_sum(index, slice_area, counts, weights):
     weighted = np.bincount(index, minlength=slice_area, weights=weights)
+    # The NaNs here represent the places with no measurements
+    weighted[counts == 0.0] = np.nan
     return weighted
