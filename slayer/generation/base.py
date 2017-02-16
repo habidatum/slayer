947d060de17e7a600fbd1fe96779aca0fb05c53b
Nikita Pestrov <ipestriov@gmail.com>
2017-02-16 13:16:46 +0300

    Don't export nan only slices

--- slayer/generation/base.py
+++ slayer/generation/base.py
@@ -108,6 +108,8 @@ class BaseGenerator:
         meta = self.shared_slison_meta()
         slisons = []
         for time_slice, slice_data in slices.items():
+            if not sum(~np.isnan(slice_data)):
+                continue
             utc_slice = time_slice.tz_convert('UTC')
             meta['timestamp'] = datetime_isoformat(utc_slice)
             slice_data_typed = slice_data.astype(self._value_type_)
